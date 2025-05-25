7bed3478c5d0fd0f92aad3ca1cca7b7826286653
kushansm <kushandileep12@mail.com>
2025-05-25 12:50:44 +0530

    add bookschema and bookcontroller

---
+++ backend/controllers/bookController.js
@@ -0,0 +1,24 @@
+const Book = require('../models/Book');
+
+// Get all books
+exports.getBooks = async (req, res) => {
+  try {
+    const books = await Book.find();
+    res.json(books);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// Create a new book
+exports.createBook = async (req, res) => {
+  const { title, author, price } = req.body;
+  const book = new Book({ title, author, price });
+
+  try {
+    const savedBook = await book.save();
+    res.status(201).json(savedBook);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
