c9a547d0553c5fb073321fcd036fa398fe9475e4
kushansm <kushandileep12@mail.com>
2025-05-26 19:57:17 +0530

    edit Readme.md

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,11 @@
 // App.jsx
 import { Routes, Route } from 'react-router-dom';
 import BookList from './pages/BookList';
-import EditBook from './pages/EditBook';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<BookList />} />
-      <Route path="/edit/:id" element={<EditBook />} />
     </Routes>
   );
 }
--- frontend/src/pages/EditBook.jsx
+++
@@ -1,8 +0,0 @@
-import { useParams } from 'react-router-dom';
-
-function EditBook() {
-  const { id } = useParams();
-  return <h1>Edit Book ID: {id}</h1>;
-}
-
-export default EditBook;
